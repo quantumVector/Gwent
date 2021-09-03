3d099a2ebac2b664e20c21a0c3c6a11ff27110a0
quantumVector <payned@mail.ru>
2021-09-04 01:04:49 +0300

    Добавлена сетка

--- src/App.js
+++ src/App.js
@@ -16,6 +16,41 @@ class App extends Component {
         {this.props.modalVisible &&
           <ModalMessage msg={this.props.modalMessage} />
         }
+        <div className="EnemyLeader"></div>
+        <div className="enemyInfo"></div>
+        <div className="weatherCards"></div>
+        <div className="userInfo"></div>
+        <div className="userLeader"></div>
+        <div className="battlefield">
+          <div className="enemySide">
+            <div className="enemyMeleeCards"></div>
+            <div className="enemyRangedCards"></div>
+            <div className="enemySiegeCards"></div>
+            <div className="enemyModifieSiege"></div>
+            <div className="enemyModifieRanged"></div>
+            <div className="enemyModifieMelee"></div>
+            <div className="enemyMeleePower"></div>
+            <div className="enemyRangedPower"></div>
+            <div className="enemySiegePower"></div>
+          </div>
+          <div className="userSide">
+            <div className="userModifierRanged"></div>
+            <div className="userModifieMelee"></div>
+            <div className="userModifierSiege"></div>
+            <div className="userMeleeCards"></div>
+            <div className="userRangedCards"></div>
+            <div className="userSiegeCards"></div>
+            <div className="userMeleePower"></div>
+            <div className="userRangedPower"></div>
+            <div className="userSiegePower"></div>
+          </div>
+          <div className="userHand"></div>
+        </div>
+        <div className="enemyGraveyard"></div>
+        <div className="userGraveyard"></div>
+        <div className="enemyDeck"></div>
+        <div className="userDeck"></div>
+        <div className="selectedCard"></div>
       </div>
     )
   }
@@ -29,3 +64,4 @@ const mapStateToProps = (state) => ({
 export default compose(
   connect(mapStateToProps, { deployGame })
 )(App);
+
